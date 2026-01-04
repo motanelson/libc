6cba9f4a2af05500305ad68287835772e98b2a12
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-04 13:35:00 +0000

    Add files via upload

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
-//gcc -m32 -nostdlib -fno-pie -no-pie -I./include ./lib/libc.o ./lib/crt.o hello.c -o hello
+//gcc -m32 -nostdlib -fno-pie -no-pie -I./include ./lib/libc.o hello.c -o hello
 #include<libc.h>
-
 int main(){
     char *c="hello world\n";
     putss(c);
-    exitss(0);
+    
     return 0;
+}
+void _start(){
+    int a=main();
+    exitss(a);
+
 }
