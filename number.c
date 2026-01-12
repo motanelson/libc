a3afd329e3d61db85be55da62163b37c6dc2f048
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-12 15:12:02 +0000

    Add files via upload

---
+++ number.c
@@ -0,0 +1,12 @@
+//gcc -m32 -nostdlib -fno-pie -no-pie -I./include ./lib/libc.o strcps.c -o strcps
+#include<libc.h>
+int main(){
+    char *c="hello world\n";
+    char cc[4096]="                                             ";
+    int n=0;
+    for(n=0;n<10;n++){
+        strs(n,cc);
+        prints(cc);
+    }
+    return 0;
+}
