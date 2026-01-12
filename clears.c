438d8a09a78a33cc4eb0bb4f90e9a288eea944ff
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-12 14:29:06 +0000

    Add files via upload

---
+++ clears.c
@@ -0,0 +1,11 @@
+//gcc -m32 -nostdlib -fno-pie -no-pie -I./include ./lib/libc.o strcps.c -o strcps
+#include<libc.h>
+int main(){
+    char *c="hello world\n";
+    char cc[4096]="                 ";
+    strncps(cc,c,6);
+    clearss();
+    prints(cc);
+    
+    return 0;
+}
