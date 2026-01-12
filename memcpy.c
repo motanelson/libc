70bba6f769f54417bddaeb42e8b176eb86ffa0b8
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-12 13:40:01 +0000

    Add files via upload

---
+++ memcpy.c
@@ -0,0 +1,10 @@
+//gcc -m32 -nostdlib -fno-pie -no-pie -I./include ./lib/libc.o strcps.c -o strcps
+#include<libc.h>
+int main(){
+    char *c="hello world\n";
+    char cc[4096]="                 ";
+    memcps(cc,c,6);
+    putss(cc);
+    
+    return 0;
+}
