d664957d8e1241312b8da2a66ef0017becbf7db6
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-12 13:53:55 +0000

    Add files via upload

--- include/libc.h
+++ include/libc.h
@@ -4,15 +4,16 @@ extern void _start();
 extern void putss(char *c);
 //extern int fputss;
 extern void exitss(int c);
-extern int strcats(char *c,char *cc);
-extern int strcps(char *c,char *cc);
-extern int strncps(char *c,char *cc,int i);
-extern int memcps(char *c,char *cc,int i);
-extern int prints(char *c);
+extern void strcats(char *c,char *cc);
+extern void strcps(char *c,char *cc);
+extern void strncps(char *c,char *cc,int i);
+extern void memcps(char *c,char *cc,int i);
+extern void prints(char *c);
+extern void memfills(char *c,int i,char cc);
+extern int strlens(char *c);
 /*
-extern int strlens
 
-extern int memfills
+
 extern int getout
 extern int getin
 extern int fgetss
